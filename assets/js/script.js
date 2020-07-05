1753d70e46dc285e23289ba2cf2a6df4b0952f7d
Loic Delmas <loichdelmas@gmail.com>
2020-07-06 01:16:24 +0200

    site benm-art

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,73 @@
 $(document).ready(function(){
-  $('.menu-tab').click(function(){
-    $('.menu-hide').toggleClass('show');
-    $('.menu-tab').toggleClass('active');
-  });
-  $('a').click(function(){
-    $('.menu-hide').removeClass('show');
-    $('.menu-tab').removeClass('active');
+    
+    $('.customer-logos').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: true,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 3
+      }
+    }]
   });
     
-//    var sitesLinks = $(".sitesLinks");
+//  $('.menu-tab').click(function(){
+//    $('.menu-hide').toggleClass('show');
+//    $('.menu-tab').toggleClass('active');
+//  });
+//  $('a').click(function(){
+//    $('.menu-hide').removeClass('show');
+//    $('.menu-tab').removeClass('active');
+//  });
 //    
-//    sitesLinks.on(mouseover, function(){
-//        if($(".site_resto").siblings.css('z-index') == 2) {
-//            $(".site_resto").css('z-index', '5');
-//        } else if($(".site_resto").siblings.css('z-index') == 3) {
-//            $(".site_resto").css('z-index', '5');
-//        } else if($(".site_resto").siblings.css('z-index') == 4) {
-//            $(".site_resto").css('z-index', '5');
-//        }
+////    var sitesLinks = $(".sitesLinks");
+////    
+////    sitesLinks.on(mouseover, function(){
+////        if($(".site_resto").siblings.css('z-index') == 2) {
+////            $(".site_resto").css('z-index', '5');
+////        } else if($(".site_resto").siblings.css('z-index') == 3) {
+////            $(".site_resto").css('z-index', '5');
+////        } else if($(".site_resto").siblings.css('z-index') == 4) {
+////            $(".site_resto").css('z-index', '5');
+////        }
+////    });
+//    
+//    $(".sitesLinks").on("mouseover", function(){
+//        var positionOnList = $(this).index() + 1;
+//        var focusedImg =  $(".imgSelect>ul>li:nth-child(" + positionOnList + ")>img");
+//        console.log(positionOnList);
+//        
+//       
+//        focusedImg.addClass("zoom");
+//        focusedImg.parent().siblings().children().removeClass("zoom");   
+//        
+//    });
+//    
+//    $(".sitesLinks").on("mouseout", function(){
+//        var positionOnList = $(this).index() + 1;
+//        var focusedImg =  $(".imgSelect>ul>li:nth-child(" + positionOnList + ")>img");
+//        console.log(positionOnList);
+//        
+//       
+//        focusedImg.removeClass("zoom");   
 //    });
     
-    $(".sitesLinks").on("mouseover", function(){
-        var positionOnList = $(this).index() + 1;
-        var focusedImg =  $(".imgSelect>ul>li:nth-child(" + positionOnList + ")>img");
-        console.log(positionOnList);
-        
-       
-        focusedImg.addClass("zoom");
-        focusedImg.parent().siblings().children().removeClass("zoom");   
-        
-    });
-    
-    $(".sitesLinks").on("mouseout", function(){
-        var positionOnList = $(this).index() + 1;
-        var focusedImg =  $(".imgSelect>ul>li:nth-child(" + positionOnList + ")>img");
-        console.log(positionOnList);
-        
-       
-        focusedImg.removeClass("zoom");   
-    });
-    
+//    $('.vitrineReeway').on("mouseover"), function() {
+//        $('.reeway').toggleClass();
+//        $('.reeway').show();
+//        console.log();
+//    }
+//    
     
 });
 
